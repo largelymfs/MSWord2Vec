38a3174b4c90b391b80e9832e2b09c64c7dbc3dc
largelyfs <largelymfs@gmail.com>
2015-02-22 16:27:13 +0800

    Add Exp Table

---
+++ ExpTable.cpp
@@ -0,0 +1,38 @@
+/* 
+* @Author: largelyfs
+* @Date:   2015-02-22 15:35:19
+* @Last Modified by:   largelyfs
+* @Last Modified time: 2015-02-22 15:45:52
+*/
+
+#include <iostream>
+#include "ExpTable.h"
+using namespace std;
+
+ExpTable::ExpTable(int MAX_TABLE_SIZE, int MAX_EXP) : elem(NULL), max_table_size(MAX_TABLE_SIZE), max_exp(MAX_EXP){
+	this->elem = new double[this->max_table_size];
+	this->init();
+}
+
+ExpTable::~ExpTable(){
+	if (elem!=NULL) delete[] elem;
+}
+
+void ExpTable::init(){
+	for (int i = 0; i < this->max_table_size; i ++){
+		this->elem[i] = exp((i / (double)(this->max_table_size) * 2.0 -1.0) * this->max_exp);
+		this->elem[i] = (this->elem[i]) / (this->elem[i] + 1.0);
+	}
+}
+
+double& ExpTable::operator[](int index){
+	return this->elem[index];
+}
+
+int main(){
+	ExpTable *e = new ExpTable(1000, 6);
+	for (int i = 0; i< 10; i++)
+		std::cout << (*e)[i] << std::endl;
+	delete e;
+    return 0;
+}
---
+++ ExpTable.h
@@ -0,0 +1,18 @@
+#ifndef EXPTABLE_H
+#define EXPTABLE_H
+
+#include <math.h>
+
+class ExpTable{
+public:
+	ExpTable(int MAX_TABLE_SIZE, int MAX_EXP);
+	void init();
+	double& operator[](int index);
+	~ExpTable();
+private:
+	double* elem;
+	int max_exp, max_table_size;
+};
+
+
+#endif
